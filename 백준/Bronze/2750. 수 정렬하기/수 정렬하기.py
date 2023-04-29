9781ab2e51bd270a86737997c9d4b0529a185abb
HaksooJI <107108021+MintZzz1009@users.noreply.github.com>
2023-04-29 10:33:38 +0900

    [Bronze II] Title: 수 정렬하기, Time: 152 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,18 +1,26 @@
-# 입력
+# input
 n = int(input())
 numbers = []
 for _ in range(n):
     num = int(input())
     numbers.append(num)
 
-
-# solution
-for _ in range(n):
-    for i in range(n - 1):
-        if numbers[i] > numbers[i + 1]:
-            numbers[i], numbers[i + 1] = numbers[i + 1], numbers[i]
-
-
-# 출력
+# solution - bubble_sort
+# for _ in range(n):
+#     for i in range(n - 1):
+#         if numbers[i] > numbers[i + 1]:
+#             numbers[i], numbers[i + 1] = numbers[i + 1], numbers[i]
+            
+# solution - selection_sort
+for start_index in range(len(numbers)):
+    min_index = start_index
+    for i in range(start_index, len(numbers)):
+        if numbers[i] < numbers[min_index]:
+            min_index = i
+            numbers[min_index] = numbers[i]
+    else:
+        numbers[start_index], numbers[min_index] = numbers[min_index], numbers[start_index]
+        
+# output
 for num in numbers:
     print(num)
