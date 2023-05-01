c379a7545666166164d8fd600b5cfbbb7c640041
HaksooJI <107108021+MintZzz1009@users.noreply.github.com>
2023-05-01 13:30:25 +0900

    [Bronze II] Title: 수 정렬하기, Time: 84 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -5,22 +5,44 @@ for _ in range(n):
     num = int(input())
     numbers.append(num)
 
-# solution - bubble_sort
-# for _ in range(n):
-#     for i in range(n - 1):
-#         if numbers[i] > numbers[i + 1]:
-#             numbers[i], numbers[i + 1] = numbers[i + 1], numbers[i]
-            
-# solution - selection_sort
-for start_index in range(len(numbers)):
-    min_index = start_index
-    for i in range(start_index, len(numbers)):
-        if numbers[i] < numbers[min_index]:
-            min_index = i
-            numbers[min_index] = numbers[i]
-    else:
-        numbers[start_index], numbers[min_index] = numbers[min_index], numbers[start_index]
+# merge_sort
+def merge_sort(array):
+    if len(array) <= 1:
+        return array
+    mid = len(array) // 2
+    array1 = merge_sort(array[:mid])
+    array2 = merge_sort(array[mid:])
+    return merge(array1, array2)
+
+def merge(array1, array2):
+    result = []
+    array1_crt_index = 0
+    array2_crt_index = 0
+    while array1_crt_index < len(array1) and array2_crt_index < len(array2):
+        if array1[array1_crt_index] < array2[array2_crt_index]:
+            result.append(array1[array1_crt_index])
+            array1_crt_index += 1
+        else:
+            result.append(array2[array2_crt_index])
+            array2_crt_index += 1
+     
+    if array1_crt_index == len(array1):
+        while array2_crt_index < len(array2):
+            result.append(array2[array2_crt_index])
+            array2_crt_index += 1
         
+    if array2_crt_index == len(array2):
+        while array1_crt_index < len(array1):
+            result.append(array1[array1_crt_index])
+            array1_crt_index += 1
+            
+    return result
+
+
+numbers = merge_sort(numbers)
+
 # output
 for num in numbers:
     print(num)
+
+    
